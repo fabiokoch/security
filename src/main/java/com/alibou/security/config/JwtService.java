c2a0785c6a2237807b2fb500dfa7c2e4cf59c88e
fabio <fabiokochdaluz@hotmail.com>
2024-11-03 13:15:17 -0300

    2 commit

---
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -0,0 +1,35 @@
+package com.alibou.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "Ei5cE9HFuvwdSIrg9d1/HXFfSAtWDFEWaFbZnteCaz/crY1GpWtKrNlargh/uALp";
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
